a5956c8eb66d69b00467ac821e1854c3b20aa0ae
Suraj singh <surajgpes@gmail.com>
2025-06-03 06:02:52 +0000

    blog page content made dynamically

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -79,11 +79,11 @@ export default async function BlogPostPage({ params }) {
         )}
 
         <div className="prose prose-lg dark:prose-invert max-w-none mx-auto text-foreground leading-relaxed">
-          <p className="text-xl mb-6">{post.excerpt}</p>
+          <p className="text-xl mb-6 font-medium">{post.excerpt}</p>
           
-          <p>This is placeholder content for the blog post titled "{post.title}". In a real application, this section would be filled with the full content of the blog post, potentially fetched from a CMS or a more detailed JSON structure.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+          {post.content && (
+            <p>{post.content}</p>
+          )}
         </div>
       </article>
     </div>
@@ -106,4 +106,3 @@ export default async function BlogPostPage({ params }) {
   .prose blockquote { @apply border-l-4 border-gray-300 dark:border-gray-600 pl-4 italic my-4 text-gray-600 dark:text-gray-400; }
 }
 */
-
